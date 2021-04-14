eecc808f3a2f659254e5c7b280400730593686b2
Leo <goncalves_leonardo@yahoo.com>
2021-04-14 20:15:20 +0100

    vuex

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -38,20 +34,20 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
-      <Login/>
-      <Register/>
+      <!-- <HelloWorld /> -->
+      <!-- <Login/> -->
+      <Register />
     </v-main>
   </v-app>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld';
-import Login from './components/views/Login';
-import Register from './components/views/Register';
+// import Login from "./components/views/Login";
+import Register from "./components/views/Register";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     //HelloWorld,

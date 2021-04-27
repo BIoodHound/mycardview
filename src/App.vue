80b7d425aae0c5fd864febeeb3dd7fe46ac4fd61
AlexisAK <alexisoalvarez94@gmail.com>
2021-04-27 10:55:12 +0100

    Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,27 @@
   <v-app>
     <v-spacer></v-spacer>
     <v-main>
-      <viewCard/>
-      <accountView/>
+      <Dashboard/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import viewCard from './components/views/viewCard';
-import accountView from './components/views/accountView';
-import Login from "./components/views/Login";
-import Register from "./components/views/Register";
+import Dashboard from './components/views/Dashboard';
+//import viewCard from './components/views/viewCard';
+//import accountView from './components/views/accountView';
+//import Login from "./components/views/Login";
+//import Register from "./components/views/Register";
 
 export default {
   name: "App",
 
   components: {
-    viewCard,
-    accountView,
-    Login,
-    Register,
+    Dashboard,
+    //viewCard,
+    //accountView,
+    //Login,
+    //Register,
   },
 
   data: () => ({
---
+++ src/components/views/Dashboard.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <v-spacer></v-spacer>
+    <v-main>
+    
+    <div class="container">
+    
+      <div class="d-flex  align-items-center justify-content-around mb-3">
+
+        <div class="col-md-6">
+          <div class="row ml-16">
+            <div>
+            <a class="mt-10 ml-15 btn border-primary btn-lg btn-block" href="#" role="button">Editar carta</a>
+            <a class="mt-10 ml-15 btn border-primary btn-lg btn-block" href="#" role="button">Luchar</a>
+            <a class="mt-10 ml-15 btn border-primary btn-lg btn-block" href="#" role="button">Mi cuenta</a>
+            </div>
+          </div>
+          
+        </div>
+
+        <div class="col-md-6">
+          <div class="p-2 bg-info">
+            <viewCard/>
+          </div>
+        </div>
+      </div>
+    </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import viewCard from './viewCard';
+
+export default {
+  name: 'Dashboard',
+  components: {
+    viewCard
+  }
+}
+</script>
